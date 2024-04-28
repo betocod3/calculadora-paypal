2a5131b88def7d44e64bef44b24b839c85f0c2be
betocodigo <irobertojrg@gmail.com>
2024-04-28 11:35:07 -0400

    livewire minified

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,1 @@
 import './bootstrap';
-import { Livewire } from '../../vendor/livewire/livewire/dist/livewire.esm'
-
-Livewire.start()
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 })->name('livewire-version');
+
+Route::get('livewire/livewire.js', function() {
+    return \Livewire\Drawer\Utils::class::pretendResponseIsFile(
+        base_path('vendor/livewire/livewire/dist/livewire.min.js')
+    );
+});
